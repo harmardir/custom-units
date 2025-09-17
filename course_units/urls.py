dd5ecf1de406d8006716d57faa2a5e70ba893b6b
Harout Mardirossian <harmardir@gmail.com>
2025-09-17 13:04:26 +0300

    updating urls

--- course_units/urls.py
+++ course_units/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Empty string "" because the base URL is already handled by PluginURLs.REGEX in apps.py
-    path("", views.units_view, name="units_view"),
+    path("units/", views.units_view, name="units_view"),
 ]
